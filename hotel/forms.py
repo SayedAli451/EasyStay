e1bf91163c94cb810507a7d3e35eea435f50bd05
SayedAli451 <sayedali4511@gmail.com>
2025-03-26 11:32:50 +0300

    .

--- hotel/forms.py
+++ hotel/forms.py
@@ -4,7 +4,7 @@ from .models import Booking , Room
 class BookingForm(forms.ModelForm):
     class Meta:
          model = Booking
-         fields = ['room', 'check_in', 'check_out', 'guests']  
+         fields = [ 'check_in', 'check_out', 'guests']  
          widgets = {
              'check_in': forms.DateInput(attrs={'type': 'date'}),
              'check_out': forms.DateInput(attrs={'type': 'date'}),
